fea459792348bc9c76c849fff3c22caaa9b14ac3
Fleen5l77 <fleen4321@outlook.com>
2024-06-14 13:28:54 +0500

    Add files via upload

--- task.js
+++ task.js
@@ -64,3 +64,36 @@ if (task2btn) {
         document.getElementById('task2').addEventListener('click', выполнитьЗадание);
     }
 }
+
+// Проверить, было ли задание уже выполнено
+if (localStorage.getItem('task3_state') === '1') {
+    // Задание уже выполнено, выйти из функции
+    var task3btn = document.getElementById("task3btn");
+if (task3btn) {
+    task3btn.innerHTML = "Задание выполнено!";
+} else {
+    console.error("Элемент с id 'task1btn' не найден на странице.");
+}
+} else {
+    // Задание еще не выполнено, продолжить выполнение задания
+    function task3() {
+        // Сохранить информацию о выполненном задании в Local Storage
+        localStorage.setItem('task3_state', '1');
+        
+        // Получить текущий баланс игрока из Local Storage
+        var balance2 = parseInt(localStorage.getItem('cookies')) || 500; // Если баланс не существует, установить значение по умолчанию 500
+        
+        // Прибавить бонус за выполненное задание к балансу
+        var bonus2 = 5000;
+        var newbalance2 = balance2 + bonus2;
+        
+        // Сохранить новый баланс игрока в Local Storage
+        localStorage.setItem('cookies', newbalance2);
+        window.location.href = 'https://youtube.com/@DenisCombat';
+    }
+
+    // Проверить, существует ли элемент 'task1' перед добавлением обработчика события
+    if (document.getElementById('task3')) {
+        document.getElementById('task3').addEventListener('click', выполнитьЗадание);
+    }
+}
